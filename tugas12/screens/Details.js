1d8aa65fa750875c2b9334d2c67b24ba50966007
EkaPratama99 <61812376+EkaPratama99@users.noreply.github.com>
2021-01-10 14:45:15 +0800

    tugas 12

---
+++ tugas12/screens/Details.js
@@ -0,0 +1,261 @@
+import React from 'react';
+import {
+  StyleSheet, Text, View, Image,TouchableOpacity
+} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#d3e3d3",  
+  },
+});
+
+const DetailsScreen = ({navigation}) => (
+  <ScrollView>
+
+  <View style={styles.container}>
+
+      <View>
+
+      <View style={{alignItems:"center", marginTop:100,}}>
+        <Text> DETAILS</Text>
+       
+      </View>
+      
+      
+      <View>
+        <Image source={require('./../screens/TS.MONA.DPN.jpg')} style={{alignItems:"center", 
+        width: 400, 
+        height: 400, 
+        marginLeft:10,
+        marginTop:50
+        }}/>
+        <Image source={require('./../screens/TS.MONA.BLKNG.jpg')} style={{alignItems:"center", 
+        width: 400, 
+        height: 400, 
+        marginLeft:10,
+        marginTop:10
+        }}/>
+       <View style={{alignItems:"center", paddingTop: 10, }}>
+        <Text style={{fontSize:20, fontWeight:"bold"}}>T-Shirt Monalisa Navy</Text>
+        <Text>Code : TS.MONALISA.NAVY</Text>
+        <Text>Rp. 55.000,-</Text>
+        <Text>Size : M, L, XL</Text>
+        <Text>----------------------------------</Text>
+        <Text> Online Order :</Text>
+        <Text>WA : 085737029266</Text>
+        <Text>Shopee : huflesssupply</Text>
+        <Text>Instagram : Huflesssupply.co</Text>
+        <View>
+
+<TouchableOpacity
+  onPress={() => navigation.navigate('fom')}
+>
+  <Text style={{ color: "white", 
+              textAlign: "center", 
+              borderRadius: 40,
+              height: 20,
+              width: 100,
+              backgroundColor: "green",
+              marginTop: 5,
+              marginBottom: 5,
+              fontSize: 15
+              }}>Beli</Text>
+</TouchableOpacity>
+</View>
+        <Text>----------------------------------------------------------------------------------------------------------------------</Text>
+        </View>
+
+      </View>
+      <View>
+        <Image source={require('./../screens/SP.HFLS.FULL.jpg')} style={{alignItems:"center", 
+        width: 400, 
+        height: 400, 
+        marginLeft:10,
+        marginTop:50
+        }}/>
+        <Image source={require('./../screens/SP.HFLS.DETAIL.jpg')} style={{alignItems:"center", 
+        width: 400, 
+        height: 400, 
+        marginLeft:10,
+        marginTop:10
+        }}/>
+       <View style={{alignItems:"center", paddingTop: 10, }}>
+        <Text style={{fontSize:20, fontWeight:"bold"}}>Short Pant Dark Grey</Text>
+        <Text>Code : HFLS.SP.DG</Text>
+        <Text>Size : 28, 30, 32, 34, 36</Text>
+        <Text>Price : Rp. 90.000,-</Text>
+        <Text>----------------------------------</Text>
+        <Text> Online Order :</Text>
+        <Text>WA : 085737029266</Text>
+        <Text>Shopee : huflesssupply</Text>
+        <Text>Instagram : Huflesssupply.co</Text>
+
+        <View>
+
+        <TouchableOpacity
+  onPress={() => navigation.navigate('fom')}
+>
+  <Text style={{ color: "white", 
+              textAlign: "center", 
+              borderRadius: 40,
+              height: 20,
+              width: 100,
+              backgroundColor: "green",
+              marginTop: 5,
+              marginBottom: 5,
+              fontSize: 15
+              }}>Beli</Text>
+</TouchableOpacity>
+</View>
+        <Text>----------------------------------------------------------------------------------------------------------------------</Text>
+        </View>
+      </View>
+      <View>
+        <Image source={require('./../screens/TS.JPN.DPN.jpg')} style={{alignItems:"center", 
+        width: 400, 
+        height: 400, 
+        marginLeft:10,
+        marginTop:50
+        }}/>
+        <Image source={require('./../screens/TS.JPN.BLKNG.jpg')} style={{alignItems:"center", 
+        width: 400, 
+        height: 400, 
+        marginLeft:10,
+        marginTop:10
+        }}/>
+       <View style={{alignItems:"center", paddingTop: 10, }}>
+        <Text style={{fontSize:20, fontWeight:"bold"}}>T-Shirt JPN Charcoal</Text>
+        <Text>Code : TS.WFONT.CRCL</Text>
+        <Text>Size : M, L, XL</Text>
+        <Text>Price : Rp. 55.000,--</Text>
+        <Text>----------------------------------</Text>
+        <Text> Online Order :</Text>
+        <Text>WA : 085737029266</Text>
+        <Text>Shopee : huflesssupply</Text>
+        <Text>Instagram : Huflesssupply.co</Text>
+
+        <View>
+
+        <TouchableOpacity
+  onPress={() => navigation.navigate('fom')}
+>
+  <Text style={{ color: "white", 
+              textAlign: "center", 
+              borderRadius: 40,
+              height: 20,
+              width: 100,
+              backgroundColor: "green",
+              marginTop: 5,
+              marginBottom: 5,
+              fontSize: 15
+              }}>Beli</Text>
+</TouchableOpacity>
+</View>
+        <Text>----------------------------------------------------------------------------------------------------------------------</Text>
+        </View>
+      </View>
+      
+
+
+      <View>
+        <Image source={require('./../screens/TKR.SKULL.FULL.jpg')} style={{alignItems:"center", 
+        width: 400, 
+        height: 400, 
+        marginLeft:10,
+        marginTop:50
+        }}/>
+        <Image source={require('./../screens/TKR.SKULL.DETAIL.jpg')} style={{alignItems:"center", 
+        width: 400, 
+        height: 400, 
+        marginLeft:10,
+        marginTop:10
+        }}/>
+       <View style={{alignItems:"center", paddingTop: 10, }}>
+        <Text style={{fontSize:20, fontWeight:"bold"}}>Topi Skull Hitam</Text>
+        <Text>Code : TKR.SKULL.BLK</Text>
+        <Text>Size : ALLSIZE</Text>
+        <Text>Price : Rp.65.000,-</Text>
+        <Text>----------------------------------</Text>
+        <Text> Online Order :</Text>
+        <Text>WA : 085737029266</Text>
+        <Text>Shopee : huflesssupply</Text>
+        <Text>Instagram : Huflesssupply.co</Text>
+
+        <View>
+
+        <TouchableOpacity
+  onPress={() => navigation.navigate('fom')}
+>
+  <Text style={{ color: "white", 
+              textAlign: "center", 
+              borderRadius: 40,
+              height: 20,
+              width: 100,
+              backgroundColor: "green",
+              marginTop: 5,
+              marginBottom: 5,
+              fontSize: 15
+              }}>Beli</Text>
+</TouchableOpacity>
+</View>
+        <Text>----------------------------------------------------------------------------------------------------------------------</Text>
+        </View>
+        </View>
+        <View>
+        <Image source={require('./../screens/HOODIE.JPN.BLK.jpg')} style={{alignItems:"center", 
+        width: 400, 
+        height: 400, 
+        marginLeft:10,
+        marginTop:50
+        }}/>
+       <View style={{alignItems:"center", paddingTop: 10, }}>
+        <Text style={{fontSize:20, fontWeight:"bold"}}>Hoodie Hitam</Text>
+        <Text>Code : HD.WFONT.BLK</Text>
+        <Text>Size : M, L, XL</Text>
+        <Text>Price : Rp.119.000,-</Text>
+        <Text>----------------------------------</Text>
+        <Text> Online Order :</Text>
+        <Text>WA : 085737029266</Text>
+        <Text>Shopee : huflesssupply</Text>
+        <Text>Instagram : Huflesssupply.co</Text>
+
+        <View>
+
+        <TouchableOpacity
+  onPress={() => navigation.navigate('fom')}
+>
+  <Text style={{ color: "white", 
+              textAlign: "center", 
+              borderRadius: 40,
+              height: 20,
+              width: 100,
+              backgroundColor: "green",
+              marginTop: 5,
+              marginBottom: 5,
+              fontSize: 15
+              }}>Beli</Text>
+</TouchableOpacity>
+</View>
+        <Text>----------------------------------------------------------------------------------------------------------------------</Text>
+        </View>
+
+        
+        </View>
+        </View>
+
+      
+
+
+
+
+
+
+    
+  </View>
+  </ScrollView>
+
+);
+
+export default DetailsScreen;
---
+++ tugas12/screens/index.js
@@ -0,0 +1,48 @@
+import LoginScreen from './Login';
+import SignupScreen from './SignUp';
+import DetailsScreen from './Details';
+import TabScreen from './Menu/BotTab';
+import HomeSreen from './Menu/Home';
+import fom from './fom'
+import Buyscreen from './Menu/Buy';
+
+module.exports = [
+
+  
+  {
+    name: 'Login',
+    component: LoginScreen,
+    options: { headerShown: false },
+  },
+  {
+    name: 'fom',
+    component: fom,
+    options: { headerShown: false },
+  },
+  {
+    name: 'SignUp',
+    component: SignupScreen,
+    options: { headerShown: false },
+  },
+  {
+    name: 'BotTab',
+    component: TabScreen,
+    options: { headerShown: false },
+  },
+
+  {
+    name: 'Details',
+    component: DetailsScreen,
+    options: { headerShown: false },
+  },
+  {
+    name: 'Home',
+    component: HomeSreen,
+    options: { headerShown: false },
+  },
+  {
+    name: 'Buy',
+    component: Buyscreen,
+    options: { headerShown: false },
+  },
+];
